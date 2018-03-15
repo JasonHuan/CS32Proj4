8b5cc4678459e647a8a0d35490c1aea45ee0841d
JasonHuan <sino@g.ucla.edu>
2018-03-14 23:56:46 -0700

    Initial Commit

---
+++ Project4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project4
+//
+//  Created by Jason Huan on 3/14/18.
+//  Copyright © 2018 Jason. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
